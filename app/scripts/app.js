27657ea6738e3ade02b9fb52c644125129fb701c
daneryl <daneryl@gmail.com>
2013-08-25 11:32:47 -0500

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('gazApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
